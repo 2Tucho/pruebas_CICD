491a3a36fac439a627b82f47207ad6278dd02a66
2Tucho <albergromero@gmail.com>
2024-05-09 11:23:53 +0200

    errors

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -1,9 +1,9 @@
 const { sum, mul, sub, div } = require('../src/math');
 
 test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2);
+  expect(sum(1, 1)).toBe(0);
 });
-test('Multiplying 1 * 1 equals 1', () => {
+
   expect(mul(1, 1)).toBe(1);
 });
 test('Subtracting 1 - 1 equals 0', () => {
